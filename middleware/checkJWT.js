ae7c39171e5c0040753f95d6b62865de09e7a33e
GoCodeyialex <jylee@gocode.sg>
2024-04-02 15:14:55 +0800

    Update middleware to check in token expire return 401

--- middleware/checkJWT.js
+++ middleware/checkJWT.js
@@ -13,9 +13,16 @@ const authMiddleware = async (req, res, next) => {
     return res.status(401).json({ message: "Unauthorized" });
   } else {
     try {
-      let user = await JWT.verify(token, "SECRET_KEY");
-      console.log(user.email);
-      req.user = user.email;
+      let tokenexp = await JWT.verify(token, "SECRET_KEY");
+      const tokenIat = new Date(tokenexp.iat * 1000);
+      const sevenDaysAgo = new Date();
+      sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
+
+      if (tokenIat < sevenDaysAgo) {
+        return res.status(401).json({ message: "Unauthorized" });
+      }
+
+      req.tokenexp = tokenexp.iat;
       next();
     } catch {
       return res.status(401).json({ message: "Unauthorized" });
